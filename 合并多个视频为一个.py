a8317e2d37286ca4f4eb2c5073b213620ab263a5
Postan <42837790+Postan-W@users.noreply.github.com>
2024-06-06 21:46:47 +0800

    sadf

--- 合并多个视频为一个.py
+++ 合并多个视频为一个.py
@@ -1,16 +1,17 @@
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 import glob
 
-video_dir = ""
-video_list =
+video_dir = "./videos/origin/"
+output_path = "./videos/output/merged_video.mp4"
 
+videos = glob.glob(video_dir+"*.mp4")
 
 # 创建视频剪辑列表
-video_clips = [VideoFileClip(video) for video in video_files]
+video_clips = [VideoFileClip(video) for video in videos]
 
 # 合并视频剪辑
 final_clip = concatenate_videoclips(video_clips, method='compose')
 
-output_path = ""
+
 # 输出合并后的视频文件
-final_clip.write_videofile('merged_video.mp4')
+final_clip.write_videofile(output_path)
