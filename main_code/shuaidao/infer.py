4dc48e73cfe9617cad3addc6eafc7a7ca7708b39
wmingdru <15810213792@163.com>
2024-06-24 17:32:04 +0800

    sa

--- main_code/shuaidao/infer.py
+++ main_code/shuaidao/infer.py
@@ -1,8 +1,9 @@
 from ultralytics import YOLO
 model = YOLO("../weights/fall_yolov8m_20240624.engine")
+print("类别有:{}".format(model.names))
 
 if __name__ == "__main__":
     input_video = "C:/Users/wmingdru/Desktop/workspace/data/shuaidao/test_video_20240624/merged/merged2.mp4"
-    results = model(input_video, save=True, stream=True, classes=[0,1,2,3,4])
+    results = model(input_video, save=True, stream=True, classes=[3])
     for result in results:
         pass
