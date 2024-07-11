f58456e57b11df16b7e4d9b32c8d4dcfa61a0589
wmingdru <15810213792@163.com>
2024-07-11 18:10:09 +0800

    pose classification

--- 文件改名.py
+++ 文件改名.py
@@ -2,10 +2,10 @@ import os
 
 
 
-dir_path = "C:/Users/wmingdru/Desktop/workspace/data/shuaidao/videos/zuoxia"
+dir_path = "C:/Users/wmingdru/Desktop/workspace/data/suzhou_videos/camera1/"
 
 files = [os.path.join(dir_path,file) for file in os.listdir(dir_path)]
 
 for i in range(len(files)):
-    os.rename(files[i],os.path.join(dir_path,"zuoxia"+str(i)+".mp4"))
+    os.rename(files[i],os.path.join(dir_path,"suzhoucamera1_"+str(i)+".avi"))
 
